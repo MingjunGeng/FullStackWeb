2ee8e2eb908fbd90e6a3a8680ebadffa35c26096
sooof <sooof@me.com>
2021-10-06 08:00:21 -0700

    React-Components-Insta-Clone

---
+++ codeLearn/LS/project/React-Components-Insta-Clone/src/components/Comments/Comments.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Comment from './Comment';
+import './Comments.css';
+
+const Comments = props => {
+  // 🔥 Make sure the parent of Comments is passing the right props!
+  const { comments } = props;
+
+  return (
+    <div>
+      {/* map through the comments prop and render a Comment for every piece of data */}
+      {
+        comments.map( comment => {
+          return <Comment
+                  key = {comment.id}
+                  comment = {comment}
+                />
+        })
+      }
+    </div>
+  );
+};
+
+export default Comments;
